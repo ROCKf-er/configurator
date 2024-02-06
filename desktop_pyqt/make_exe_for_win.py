d988886d583e8d8d5cf5d727a206711cf2fa81c2
SergeyYTS <sergey.y@techspark.engineering>
2024-02-06 11:29:14 +0400

    KMKZ-29 Fix.

--- desktop_pyqt/make_exe_for_win.py
+++ desktop_pyqt/make_exe_for_win.py
@@ -8,7 +8,8 @@ print("Remove folder \"build\"")
 os.system('rmdir /s /q build')
 
 print("Make exe")
-os.system('python -m PyInstaller --clean --noconfirm --add-data "./../parameters.xml;./src" --add-data="./src/*.*;./src" --hidden-import=pymavlink.dialects.v20 --hidden-import=pymavlink.dialects.v20.ardupilotmega --distpath "./dist_windows" --onefile configurator.py')
+#os.system('python -m PyInstaller --clean --noconfirm --add-data "./../parameters.xml;./src" --add-data="./src/*.*;./src" --hidden-import=pymavlink.dialects.v20 --hidden-import=pymavlink.dialects.v20.ardupilotmega --distpath "./dist_windows" --onefile configurator.py')
+os.system('pyinstaller --clean --noconfirm --add-data "./../parameters.xml;./src" --add-data="./src/*.*;./src" --hidden-import=pymavlink.dialects.v20 --hidden-import=pymavlink.dialects.v20.ardupilotmega --distpath "./dist_windows" --onefile configurator.py')
 print("Make Exe done.")
 
 t = datetime.datetime.now()
