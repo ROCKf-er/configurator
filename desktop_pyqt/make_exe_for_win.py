4fd463d3aa192421c90cc2775591cc66af0f2177
SergeyYTS <sergey.y@techspark.engineering>
2024-02-06 11:18:59 +0400

    KMKZ-29 Exe for make exe.

---
+++ desktop_pyqt/make_exe_for_win.py
@@ -0,0 +1,21 @@
+# pyinstaller --clean --noconfirm --onefile --distpath "./" make_exe_for_win.py
+import os
+import datetime
+
+#check_output("dir C:", shell=True)
+print("Remove folder \"build\"")
+#ret = check_output("rmdir /s build", shell=True)
+os.system('rmdir /s /q build')
+
+print("Make exe")
+os.system('python -m PyInstaller --clean --noconfirm --add-data "./../parameters.xml;./src" --add-data="./src/*.*;./src" --hidden-import=pymavlink.dialects.v20 --hidden-import=pymavlink.dialects.v20.ardupilotmega --distpath "./dist_windows" --onefile configurator.py')
+print("Make Exe done.")
+
+t = datetime.datetime.now()
+ymd_str = t.strftime('%Y%m%d')
+
+print('Change dir to "dist_windows"')
+os.chdir('dist_windows')
+comm = f'ren configurator.exe configurator_{ymd_str}.exe'
+print(comm)
+os.system(comm)
