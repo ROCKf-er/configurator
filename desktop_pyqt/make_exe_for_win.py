a4518179a52c0dd45254c63d669488e4067c40bf
SergeyYTS <sergey.y@techspark.engineering>
2024-02-23 16:24:40 +0400

    KMKZ-29 Make win exe in one directory.

--- desktop_pyqt/make_exe_for_win.py
+++ desktop_pyqt/make_exe_for_win.py
@@ -7,16 +7,16 @@ print("Remove folder \"build\"")
 #ret = check_output("rmdir /s build", shell=True)
 os.system('rmdir /s /q build')
 
+t = datetime.datetime.now()
+ymd_str = t.strftime('%Y%m%d%H%M')
+
 print("Make exe")
 #os.system('python -m PyInstaller --clean --noconfirm --add-data "./../parameters.xml;./src" --add-data="./src/*.*;./src" --hidden-import=pymavlink.dialects.v20 --hidden-import=pymavlink.dialects.v20.ardupilotmega --distpath "./dist_windows" --onefile configurator.py')
-os.system('pyinstaller --clean --noconfirm --add-data "./../parameters.xml;./src" --add-data="./src/*.*;./src" --hidden-import=pymavlink.dialects.v20 --hidden-import=pymavlink.dialects.v20.ardupilotmega --distpath "./dist_windows" --onefile configurator.py')
+os.system(f'pyinstaller --clean --noconfirm --add-data "./../parameters.xml;./src" --add-data="./src/*.*;./src" --hidden-import=pymavlink.dialects.v20 --hidden-import=pymavlink.dialects.v20.ardupilotmega --distpath "./dist_windows/configurator_{ymd_str}" --onedir configurator.py')
 print("Make Exe done.")
 
-t = datetime.datetime.now()
-ymd_str = t.strftime('%Y%m%d')
-
-print('Change dir to "dist_windows"')
-os.chdir('dist_windows')
-comm = f'ren configurator.exe configurator_{ymd_str}.exe'
-print(comm)
-os.system(comm)
+# print('Change dir to "dist_windows"')
+# os.chdir('dist_windows')
+# comm = f'ren configurator.exe configurator_{ymd_str}.exe'
+# print(comm)
+# os.system(comm)
