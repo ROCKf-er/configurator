98b9622ba7d32ef4502dd1630b933c732487954d
SergeyYTS <sergey.y@techspark.engineering>
2024-03-11 12:11:30 +0400

    KMKZ2-29 Fix folder structure for windows executable.

--- desktop_pyqt/make_exe_for_win.py
+++ desktop_pyqt/make_exe_for_win.py
@@ -12,12 +12,19 @@ ymd_str = t.strftime('%Y%m%d%H%M')
 
 print("Make exe")
 #os.system('python -m PyInstaller --clean --noconfirm --add-data "./../parameters.xml;./src" --add-data="./src/*.*;./src" --hidden-import=pymavlink.dialects.v20 --hidden-import=pymavlink.dialects.v20.ardupilotmega --distpath "./dist_windows" --onefile configurator.py')
-os.system(f'pyinstaller --clean --noconfirm --add-data "./../parameters.xml;./src" --add-data="./src/*.*;./src" --hidden-import=pymavlink.dialects.v20 --hidden-import=pymavlink.dialects.v20.ardupilotmega --distpath "./dist_windows/configurator_{ymd_str}" --onedir configurator.py')
+os.system(f'pyinstaller --clean --noconfirm --add-data "./../parameters.xml;./src" --add-data="./src/*.*;./src" --hidden-import=pymavlink.dialects.v20 --hidden-import=pymavlink.dialects.v20.ardupilotmega --distpath "./dist_windows" --onedir configurator.py')
 print("Make Exe done.")
 
-print("Add lnk to exe.")
-comm = f'xcopy .\dist_windows_lnk\configurator.exe.lnk .\dist_windows\configurator_{ymd_str}'
+print('Change dir to: dist_windows')
+os.chdir('dist_windows')
+
+print(f"Rename folder to: configurator_{ymd_str}")
+comm = f'ren "configurator" "configurator_{ymd_str}"'
 os.system(comm)
+
+# print("Add lnk to exe.")
+# comm = f'xcopy .\dist_windows_lnk\configurator.exe.lnk .\dist_windows\configurator_{ymd_str}'
+# os.system(comm)
 
 # print('Change dir to "dist_windows"')
 # os.chdir('dist_windows')
